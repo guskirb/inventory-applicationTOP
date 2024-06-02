128eb5dda2fe121a474f58ae01a3950d9a283b35
guskirb <anguskirby1998@gmail.com>
2024-06-02 23:40:20 +0100

    Add empty controllers and routes

---
+++ controllers/formatController.js
@@ -0,0 +1,34 @@
+const Format = require('../models/format');
+const asyncHandler = require('express-async-handler');
+
+exports.format_list = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_detail = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_create_get = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_create_post = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_delete_get = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_delete_post = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_update_get = asyncHandler(async(req, res, next) => {
+    
+});
+
+exports.format_update_post = asyncHandler(async(req, res, next) => {
+    
+});
---
+++ routes/category.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router();
+
+const album_controller = require('../controllers/albumController');
+const artist_controller = require('../controllers/artistController');
+const format_controller = require('../controllers/formatController');
+const genre_controller = require('../controllers/genreController');
+const label_controller = require('../controllers/labelController');
+const index_controller = require('../controllers/indexController');
+
+// Homepage route
+router.get('/', index_controller.index);
+
+// Album routes
+router.get('/albums', album_controller.album_list);
+
+router.get('/album/:id', album_controller.album_detail);
+
+router.get('/album/create', album_controller.album_create_get);
+
+router.post('/album/create', album_controller.album_create_post);
+
+router.get('/album/:id/delete', album_controller.album_delete_get);
+
+router.post('/album/:id/delete', album_controller.album_create_post);
+
+router.get('/album/:id/update', album_controller.album_update_get);
+
+router.post('/album/:id/update', album_controller.album_update_post);
+
+// Artist routes
+router.get('/artists', artist_controller.artist_list);
+
+router.get('/artist/:id', artist_controller.artist_detail);
+
+router.get('/artist/create', artist_controller.artist_create_get);
+
+router.post('/artist/create', artist_controller.artist_create_post);
+
+router.get('/artist/:id/delete', artist_controller.artist_delete_get);
+
+router.post('/artist/:id/delete', artist_controller.artist_create_post);
+
+router.get('/artist/:id/update', artist_controller.artist_update_get);
+
+router.post('/artist/:id/update', artist_controller.artist_update_post);
+
+// Format routes
+router.get('/releases', format_controller.format_list);
+
+router.get('/release/:id', format_controller.format_detail);
+
+router.get('/release/create', format_controller.format_create_get);
+
+router.post('/release/create', format_controller.format_create_post);
+
+router.get('/release/:id/delete', format_controller.format_delete_get);
+
+router.post('/release/:id/delete', format_controller.format_create_post);
+
+router.get('/release/:id/update', format_controller.format_update_get);
+
+router.post('/release/:id/update', format_controller.format_update_post);
+
+// Genre list
+router.get('/genres', genre_controller.genre_list);
+
+router.get('/genre/:id', genre_controller.genre_detail);
+
+router.get('/genre/create', genre_controller.genre_create_get);
+
+router.post('/genre/create', genre_controller.genre_create_post);
+
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
+
+router.post('/genre/:id/delete', genre_controller.genre_create_post);
+
+router.get('/genre/:id/update', genre_controller.genre_update_get);
+
+router.post('/genre/:id/update', genre_controller.genre_update_post);
+
+// Label routes
+router.get('/labels', label_controller.label_list);
+
+router.get('/label/:id', label_controller.label_detail);
+
+router.get('/label/create', label_controller.label_delete_get);
+
+router.post('/label/create', label_controller.label_create_post);
+
+router.get('/label/:id/delete', label_controller.label_delete_get);
+
+router.post('/label/:id/delete', label_controller.label_create_post);
+
+router.get('/label/:id/update', label_controller.label_update_get);
+
+router.post('/label/:id/update', label_controller.label_update_post);

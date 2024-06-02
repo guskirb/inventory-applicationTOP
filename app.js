ef95a6daf324e8b1494017d249e3e608d4c73346
guskirb <anguskirby1998@gmail.com>
2024-06-03 00:28:05 +0100

    Remove users route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -8,18 +9,18 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const mongoDB = 'mongodb+srv://guskirb:WG6emFaMRwQ3ZcqW@cluster0.xnhxuem.mongodb.net/music_inventory?retryWrites=true&w=majority&appName=Cluster0'
 
+const indexRouter = require('./routes/index');
+const categoryRouter = require('./routes/category');
+
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const categoryRouter = require('./routes/category');
-
 const app = express();
 
 // view engine setup
+app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -30,7 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
 
 // catch 404 and forward to error handler

47707b3ebf7a7b2951b62e89b4493254d1feeff2
guskirb <anguskirby1998@gmail.com>
2024-06-04 20:16:01 +0100

    Add format post method & index controller

--- controllers/formatController.js
+++ controllers/formatController.js
@@ -1,6 +1,7 @@
 const Format = require('../models/format');
 const Album = require('../models/album');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 exports.format_list = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED");
@@ -11,7 +12,7 @@ exports.format_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.format_create_get = asyncHandler(async (req, res, next) => {
-    const allAlbums = await Album.find().sort({ title: 1 }).exec();
+    const allAlbums = await Album.find().sort({ title: 1 }).populate('artist').exec();
 
     res.render('format_form', {
         title: 'Add Release',
@@ -20,9 +21,41 @@ exports.format_create_get = asyncHandler(async (req, res, next) => {
     });
 });
 
-exports.format_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED");
-});
+exports.format_create_post = [
+    body('album', 'Album must be specified').trim().isLength({ min: 1 }).escape(),
+    body('format', 'Format must be specified.').escape(),
+    body('price', 'Price must be specified.').isNumeric().withMessage('Only Decimals allowed'),
+    body('stock', 'Stock amount must be specified.').isNumeric().withMessage('Only Decimals allowed'),
+    body('barcode', 'Barcode must be specified')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const format = new Format({
+            album: req.body.album,
+            format: req.body.format,
+            price: req.body.price,
+            stock: req.body.stock,
+            barcode: req.body.barcode,
+        });
+
+        if (!errors.isEmpty()) {
+            const allAlbums = await Album.find().sort({ title: 1 }).populate('artist').exec();
+
+            res.render('format_form', {
+                title: 'Add Release',
+                format: format,
+                albums: allAlbums,
+                errors: errors.array(),
+            });
+        } else {
+            await format.save();
+            res.redirect(format.url);
+        }
+    }),
+];
 
 exports.format_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED");
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,18 @@
 const asyncHandler = require('express-async-handler');
+const Album = require('../models/album');
+const Format = require('../models/format');
 
 exports.index = asyncHandler(async (req, res, next) => {
+    const [allAlbums, allFormats] = await Promise.all([
+        Album.countDocuments({}).exec(),
+        Format.aggregate([
+            { $group: { _id: null, stock: { $sum: "$stock" } } }
+        ]).exec(),
+    ]);
+    
     res.render('index', {
-        title: 'Music Inventory'
+        title: 'Music Inventory',
+        albums: allAlbums,
+        formats: allFormats,
     })
 });

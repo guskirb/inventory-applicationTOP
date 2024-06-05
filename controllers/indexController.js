5ca78f4fa0828e0d03d1e414bdcfaf476130ef14
guskirb <anguskirby1998@gmail.com>
2024-06-05 03:00:08 +0100

    Add low stock aggregation pipeline

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,10 +6,16 @@ exports.index = asyncHandler(async (req, res, next) => {
     const [allAlbums, allFormats] = await Promise.all([
         Album.countDocuments({}).exec(),
         Format.aggregate([
-            { $group: { _id: null, stock: { $sum: "$stock" } } }
+            {
+                $group: {
+                    _id: null,
+                    stock: { $sum: "$stock" },
+                    low_stock: { $sum: { $cond: [{ $lte: ['$stock', 5] }, 1, 0] } },
+                }
+            },
         ]).exec(),
     ]);
-    
+    console.log(allFormats)
     res.render('index', {
         title: 'Music Inventory',
         albums: allAlbums,
